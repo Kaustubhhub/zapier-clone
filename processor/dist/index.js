566c13e59c774cc41c43d93aac14a5ca7223c19e
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-07-26 23:35:31 +0530

    kafka-processor

---
+++ processor/dist/index.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("processor backend");
---
+++ processor/src/index.ts
@@ -0,0 +1,10 @@
+
+import { PrismaClient } from '@prisma/client'
+const client = new PrismaClient();
+
+async function main() {
+    while (1) {
+    }
+}
+
+main()
