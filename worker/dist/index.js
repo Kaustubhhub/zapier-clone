96d8daa678a50f94e9500ca22b5ad415930948fe
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-07-28 16:11:11 +0530

    autocommit flase

---
+++ worker/dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const kafkajs_1 = require("kafkajs");
+const kafka = new kafkajs_1.Kafka({
+    clientId: "outbox-processor",
+    brokers: ["localhost:9092"]
+});
+const TOPIC_NAME = 'zap-events';
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const consumer = kafka.consumer({ groupId: 'main-worker' });
+        yield consumer.connect();
+        yield consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+        yield consumer.run({
+            eachMessage: ({ topic, partition, message }) => __awaiter(this, void 0, void 0, function* () {
+                console.log({
+                    partition,
+                    offset: message.offset,
+                    message: message.value,
+                });
+                yield new Promise(r => setTimeout(r, 1000));
+            })
+        });
+    });
+}
+main();
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -12,12 +12,14 @@ async function main() {
 
     await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true })
     await consumer.run({
+        autoCommit: false,
         eachMessage: async ({ topic, partition, message }) => {
             console.log({
                 partition,
                 offset: message.offset,
                 message: message.value,
             });
+            await new Promise(r => setTimeout(r, 1000));
         }
     })
 
