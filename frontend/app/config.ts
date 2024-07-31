19e52a92462604f2a1ad8e0f96b24d410e31e6f2
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-07-31 23:23:31 +0530

    dashboard zaps table

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,1 +1,2 @@
 export const BACKEND_URL = `http://localhost:3001/api/v1`;
+export const HOOKS_URL = `http://localhost:3000`;
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,8 +1,58 @@
 "use client";
 import { Appbar } from "@/components/Appbar";
 import { CreateButton } from "@/components/buttons/CreateButton";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL, HOOKS_URL } from "../config";
+import { LinkButton } from "@/components/buttons/LinkButton";
+import { useRouter } from "next/navigation";
+interface Zap {
+    "id": string,
+    "triggerId": string,
+    "userId": number,
+    "actions": {
+        "id": string,
+        "zapId": string,
+        "actionId": string,
+        "sortingOrder": number,
+        "type": {
+            "id": string,
+            "name": string
+            "image": string
+        }
+    }[],
+    "trigger": {
+        "id": string,
+        "zapId": string,
+        "triggerId": string,
+        "type": {
+            "id": string,
+            "name": string,
+            "image": string
+        }
+    }
+}
+
+function useZaps() {
+    const [loading, setLoading] = useState(true);
+    const [zaps, setZaps] = useState<Zap[]>([])
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/zap`, {
+            headers: {
+                Authorization: localStorage.getItem('token')
+            }
+        }).then(res => {
+            setZaps(res.data.zaps);
+            setLoading(false);
+        })
+    }, [])
+
+    return { loading, zaps }
+}
 
 export default function () {
+    const { loading, zaps } = useZaps();
     return <div>
         <Appbar />
         <div className="flex justify-around pt-10">
@@ -11,9 +61,33 @@ export default function () {
             </div>
             <div>
                 <CreateButton onClick={() => { }} >
-                   <span>+</span> create
+                    <span>+</span> create
                 </CreateButton>
             </div>
         </div>
+        {loading ? "loading..." : <div className="flex justify-center"> <ZapTable zaps={zaps} /> </div>}
+    </div>
+}
+
+function ZapTable({ zaps }: { zaps: Zap[] }) {
+    const router = useRouter();
+
+    return <div className="p-8 max-w-screen-lg w-full">
+        <div className="flex">
+            <div className="flex-1">Name</div>
+            <div className="flex-1">ID</div>
+            <div className="flex-1">Created at</div>
+            <div className="flex-1">Webhook URL</div>
+            <div className="flex-1">Go</div>
+        </div>
+        {zaps.map(z => <div className="flex border-b border-t py-4">
+            <div className="flex-1 flex"><img src={z.trigger.type.image} className="w-[30px] h-[30px]" /> {z.actions.map(x => <img src={x.type.image} className="w-[30px] h-[30px]" />)}</div>
+            <div className="flex-1">{z.id}</div>
+            <div className="flex-1">Nov 13, 2023</div>
+            <div className="flex-1">{`${HOOKS_URL}/hooks/catch/1/${z.id}`}</div>
+            <div className="flex-1"><LinkButton onClick={() => {
+                router.push("/zap/" + z.id)
+            }}>Go</LinkButton></div>
+        </div>)}
     </div>
 }
