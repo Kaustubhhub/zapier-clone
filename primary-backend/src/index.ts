3a319e86ffc19f0d7d7e83bd8eff06620fe72a82
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-08-02 01:26:32 +0530

    trigger-action init

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -2,12 +2,16 @@ import express from 'express'
 import { userRouter } from './router/user';
 import { zapRouter } from './router/zap';
 import cors from 'cors'
+import { actionRouter } from './router/action';
+import { triggerRouter } from './router/trigger';
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/zap', zapRouter);
+app.use('/api/v1/trigger', triggerRouter);
+app.use('/api/v1/action', actionRouter);
 
 app.listen(3001, () => {
     console.log(`primary backend is running on port 3001`)
